081743965be3ca8e8593f987c141f6a3517b3c10
Leow Kean Tat <leowkeantat@gmail.com>
2024-02-10 03:16:38 +0800

    Applied functions utilizing prop parameters to the resume page's groups.

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,54 @@ import './App.css'
 import profile from "./assets/profile.jpg";
 // =====================================================
 // Left Side of the Page
+function LeftScreenHeader(header) {
+  return (
+    <p className="left-screen-header">
+      {header}
+    </p>
+  );
+}
+
+function LeftScreenContent(content) {
+  return (
+    <p className="left-screen-content">
+      {content}
+    </p>
+  );
+}
+
+function LeftScreenContentDescription(contentDescription) {
+  return (
+    <p className="left-screen-content-description">
+      {contentDescription}
+    </p>
+  );
+}
+
+function LeftScreenHeaderTitle(title) {
+  return (
+    <p className="left-screen-header-title">
+      {title}
+    </p>
+  );
+}
+
+function LeftScreenHeaderQuote(quote) {
+  return (
+    <p className="left-screen-content-quote">
+      {quote}
+    </p>
+  );
+}
+
+function LeftScreenHeaderListItem(listItem) {
+  return (
+    <li className="left-screen-content-description">
+      {listItem}
+    </li>
+  );
+}
+
 function Title() {
   return (
     <>
@@ -34,12 +82,8 @@ function ProfileSummary() {
     <>
       <Row className="left-screen-group">
         <Col>
-          <p className="left-screen-header">
-            Profile Summary
-          </p>
-          <p className="left-screen-content">
-            Game Programmer and Full Stack Developer hybrid. Intrinsically finds value in creating codebases which balances between code readability and respecting the big O notation.
-          </p>
+          {LeftScreenHeader("Profile Summary")}
+          {LeftScreenContent("Game Programmer and Full Stack Developer hybrid. Intrinsically finds value in creating codebases which balances between code readability and respecting the big O notation.")}
         </Col>
       </Row>
     </>
@@ -51,81 +95,46 @@ function EmploymentHistory() {
     <>
       <Row className="left-screen-group">
         <Col>
-          <p className="left-screen-header">
-            Employment History
-          </p>
-          <p className="left-screen-header-title">
-            Senior Game Programmer, Kotakoren Games Studio, Remote
-          </p>
-          <p className="left-screen-content-quote">
-            APRIL 2022 - JUNE 2023
-          </p>
+          {LeftScreenHeader("Employment History")}
+
+          {LeftScreenHeaderTitle("Senior Game Programmer, Kotakoren Games Studio, Remote")}
+          {LeftScreenHeaderQuote("APRIL 2022 - JUNE 2023")}
 
           <ul>
-            <li className="left-screen-content-description">
-              Developed sample projects and demos which accelerated and enabled the junior programmers' ability to visualize intermediate programming concepts.
-            </li>
-            <li className="left-screen-content-description">
-              Developed a set of assessment tests for the company which has helped to completely eliminate the potentiality of hiring under-qualified applicants.
-            </li>
-            <li className="left-screen-content-description">
-              Developed Unity custom editor tools to aid the art team with their assets' integration from their preferred 3D Modeling Software into the Unity Engine and macros to optimise their assets on import. Significantly reduced the time taken for them to set up each Look Dev scene for just an upfront cost of 1 to 2 weeks in tool research and development time.
-            </li>
-            <li className="left-screen-content-description">
-              Successfully brought the company project out from development limbo and produced a playable prototype.
-            </li>
+            {LeftScreenHeaderListItem("Developed sample projects and demos which accelerated and enabled the junior programmers' ability to visualize intermediate programming concepts.")}
+            {LeftScreenHeaderListItem("Developed a set of assessment tests for the company which has helped to completely eliminate the potentiality of hiring under-qualified applicants.")}
+            {LeftScreenHeaderListItem("Developed Unity custom editor tools to aid the art team with their assets' integration from their preferred 3D Modeling Software into the Unity Engine and macros to optimise their assets on import. Significantly reduced the time taken for them to set up each Look Dev scene for just an upfront cost of 1 to 2 weeks in tool research and development time.")}
+            {LeftScreenHeaderListItem("Successfully brought the company project out from development limbo and produced a playable prototype.")}
           </ul>
 
           <br />
 
-          <p className="left-screen-header-title">
-            Senior Game Programmer, Soulofox Studio, Remote
-          </p>
-          <p className="left-screen-content-quote">
-            FEBRUARY 2022 - MARCH 2022
-          </p>
+          {LeftScreenHeaderTitle("Senior Game Programmer, Soulofox Studio, Remote")}
+          {LeftScreenHeaderQuote("FEBRUARY 2022 - MARCH 2022")}
 
           <ul>
-            <li className="left-screen-content-description">
-              Developed a minimalist yet reusable system which supports animations from different sprite engines (DragonBones, Spine) which significantly reduced the time it took to quickly test art assets from various outsourced teams in the Look Dev scene.
-            </li>
-            <li className="left-screen-content-description">
-              Assisted in researching for solutions to integrate the web camera into Unity's WebGL Platform.
-            </li>
+            {LeftScreenHeaderListItem("Developed a minimalist yet reusable system which supports animations from different sprite engines (DragonBones, Spine) which significantly reduced the time it took to quickly test art assets from various outsourced teams in the Look Dev scene.")}
+            {LeftScreenHeaderListItem("Assisted in researching for solutions to integrate the web camera into Unity's WebGL Platform.")}
           </ul>
 
           <br />
 
-          <p className="left-screen-header-title">
-            Junior Full Stack Developer Liquid Rock Games Studio, Remote
-          </p>
-          <p className="left-screen-content-quote">AUGUST 2017 - AUGUST 2019</p>
+          {LeftScreenHeaderTitle("Junior Full Stack Developer Liquid Rock Games Studio, Remote")}
+          {LeftScreenHeaderQuote("AUGUST 2017 - AUGUST 2019")}
 
           <ul>
-            <li className="left-screen-content-description">
-              Was part of the team which launched the e-hailing app in Malaysia, known as "CarriageForHer". Its development process has spearheaded my learning process on both how the technology stack in this field communicates with one another and Google Services' Integration into both React Native and Laravel Frameworks.
-            </li>
-            <li className="left-screen-content-description">
-              Was also a part of an unnamed e-commerce project. This gave me the opportunity to learn how to integrate third-party transaction services in the client app.
-            </li>
+            {LeftScreenHeaderListItem("Was part of the team which launched the e-hailing app in Malaysia, known as \"CarriageForHer\". Its development process has spearheaded my learning process on both how the technology stack in this field communicates with one another and Google Services' Integration into both React Native and Laravel Frameworks.")}
+            {LeftScreenHeaderListItem("Was also a part of an unnamed e-commerce project. This gave me the opportunity to learn how to integrate third-party transaction services in the client app.")}
           </ul>
 
           <br />
 
-          <p className="left-screen-header-title">
-            Junior Game Programmer, Techninier Sdn Bhd, Q Sentral, 2A, Jalan Stesen Sentral 2, Kuala Lumpur Sentral, 50470 Kuala Lumpur (On-site)
-          </p>
-          <p className="left-screen-content-quote">
-            AUGUST 2016 - JANUARY 2017
-          </p>
+          {LeftScreenHeaderTitle("Junior Game Programmer, Techninier Sdn Bhd, Q Sentral, 2A, Jalan Stesen Sentral 2, Kuala Lumpur Sentral, 50470 Kuala Lumpur (On-site)")}
+          {LeftScreenHeaderQuote("AUGUST 2016 - JANUARY 2017")}
 
           <ul>
-            <li className="left-screen-content-description">
-              Successfully developed the company's unnamed prototype's (2D MOBA genre) core feature sets in 3 months (ahead of the original schedule of ~6 months).
-            </li>
-            <li className="left-screen-content-description">
-              Aided the game designer's game balancing process by creating easy-to-navigate-and-use (minimalist UI design and K.I.S.S approach) custom editor tools written in C# in the Unity Engine.
-            </li>
+            {LeftScreenHeaderListItem("Successfully developed the company's unnamed prototype's (2D MOBA genre) core feature sets in 3 months (ahead of the original schedule of ~6 months).")}
+            {LeftScreenHeaderListItem("Aided the game designer's game balancing process by creating easy-to-navigate-and-use (minimalist UI design and K.I.S.S approach) custom editor tools written in C# in the Unity Engine.")}
           </ul>
         </Col>
       </Row>
@@ -138,21 +147,12 @@ function TrainingHistory() {
     <>
       <Row className="left-screen-group">
         <Col>
-          <p className="left-screen-header">
-            Internships/Trainings
-          </p>
-          <p className="left-screen-header-title">
-            Game Programmer at AppAsia Studio, E-5-4, 5th Floor, Megan Avenue 1, Jalan Tun Razak., Kuala Lumpur, Kuala Lumpur 50400, MY
-          </p>
-          <p className="left-screen-content-quote">
-            MAY 2016 - AUGUST 2016
-          </p>
+          {LeftScreenHeader("Internships/Trainings")}
+          {LeftScreenHeaderTitle("Game Programmer at AppAsia Studio, E-5-4, 5th Floor, Megan Avenue 1, Jalan Tun Razak., Kuala Lumpur, Kuala Lumpur 50400, MY")}
+          {LeftScreenHeaderQuote("MAY 2016 - AUGUST 2016")}
 
           <ul>
-            <li className="left-screen-content-description">
-              Worked mainly on mobile platform games using
-              the Unity Engine. Was also involved in the post-production, especially the deployment process.
-            </li>
+            {LeftScreenHeaderListItem("Worked mainly on mobile platform games using the Unity Engine.Was also involved in the post - production, especially the deployment process.")}
           </ul>
         </Col>
       </Row>
@@ -165,30 +165,16 @@ function EducationHistory() {
     <>
       <Row className="left-screen-group">
         <Col>
-          <p className="left-screen-header">
-            Education
-          </p>
-          <p className="left-screen-header-title">
-            Bachelor's Degree in Game Development (Programming), UOW Malaysia KDU University College, Jalan Kontraktor U1/14, Glenpark U1, 40150 Shah Alam, Selangor
-          </p>
-          <p className="left-screen-content-quote">
-            APRIL 2013 - OCTOBER 2016
-          </p>
-          <p className="left-screen-content-description">
-            First Class
-          </p>
+          {LeftScreenHeader("Education")}
+          {LeftScreenHeaderTitle("Bachelor's Degree in Game Development (Programming), UOW Malaysia KDU University College, Jalan Kontraktor U1/14, Glenpark U1, 40150 Shah Alam, Selangor")}
+          {LeftScreenHeaderQuote("APRIL 2013 - OCTOBER 2016")}
+          {LeftScreenContentDescription("First Class")}
 
           <br />
 
-          <p className="left-screen-header-title">
-            Diploma of Education in Games Development, UOW Malaysia KDU Penang University College, 32, Jalan Anson, George Town, 10400 George Town, Pulau Pinang
-          </p>
-          <p className="left-screen-content-quote">
-            JANUARY 2011 - APRIL 2013
-          </p>
-          <p className="left-screen-content-description">
-            Distinction
-          </p>
+          {LeftScreenHeaderTitle("Diploma of Education in Games Development, UOW Malaysia KDU Penang University College, 32, Jalan Anson, George Town, 10400 George Town, Pulau Pinang")}
+          {LeftScreenHeaderQuote("JANUARY 2011 - APRIL 2013")}
+          {LeftScreenContentDescription("Distinction")}
         </Col>
       </Row>
     </>
@@ -196,27 +182,42 @@ function EducationHistory() {
 }
 // =====================================================
 // Right Side of the Page
+function RightScreenHeader(header) {
+  return (
+    <p className="right-screen-header">
+      {header}
+    </p>
+  );
+}
+
+function RightScreenContent(content) {
+  return (
+    <p className="right-screen-content">
+      {content}
+    </p>
+  );
+}
+
+function RightScreenContentLink(href, link) {
+  return (
+    <p className="right-screen-content">
+      <a href={href} className="right-screen-content-link">
+        {link}
+      </a>
+    </p>
+  );
+}
+
 function ContactDetails() {
   return (
     <>
       <Row className="right-screen-group">
         <Col>
-          <p className="right-screen-header">
-            Details
-          </p>
-
-          <p className="right-screen-content">
-            Bayan Lepas, Penang
-          </p>
-          <p className="right-screen-content">
-            Malaysia
-          </p>
-          <p className="right-screen-content">
-            +(60)17-5845732
-          </p>
-          <a href={"mailto:leowkeantat@gmail.com"} className="right-screen-content">
-            leowkeantat@gmail.com
-          </a>
+          {RightScreenHeader("Details")}
+          {RightScreenContent("Bayan Lepas, Penang")}
+          {RightScreenContent("Malaysia")}
+          {RightScreenContent("+(60)17-5845732")}
+          {RightScreenContentLink("mailto:leowkeantat@gmail.com", "leowkeantat@gmail.com")}
         </Col>
       </Row>
     </>
@@ -228,20 +229,9 @@ function Links() {
     <>
       <Row className="right-screen-group">
         <Col>
-          <p className="right-screen-header">
-            Links
-          </p>
-
-          <p className="right-screen-content">
-            <a href={"https://www.linkedin.com/in/kazcade/"} target={"_blank"} rel="noopener noreferrer" className="right-screen-content-link">
-              LinkedIn
-            </a>
-          </p>
-          <p className="right-screen-content">
-            <a href={"https://projectkazcade.itch.io/"} target={"_blank"} rel="noopener noreferrer" className="right-screen-content-link">
-              Itch IO
-            </a>
-          </p>
+          {RightScreenHeader("Links")}
+          {RightScreenContentLink("https://www.linkedin.com/in/kazcade/", "LinkedIn")}
+          {RightScreenContentLink("https://projectkazcade.itch.io/", "Itch IO")}
         </Col>
       </Row>
     </>
@@ -253,61 +243,30 @@ function Skills() {
     <>
       <Row className="right-screen-group">
         <Col>
-          <p className="right-screen-header">
-            Skills
-          </p>
-
-          <p className="right-screen-content">
-            Unity Game Engine
-          </p>
-          <p className="right-screen-content">
-            Unreal Engine (Blueprint)
-          </p>
+          {RightScreenHeader("Skills")}
+          {RightScreenContent("Unity Game Engine")}
+          {RightScreenContent("Unreal Engine (Blueprint)")}
 
           <br />
 
-          <p className="right-screen-content">
-            Perforce P4V Client
-          </p>
-          <p className="right-screen-content">
-            GIT/SVN
-          </p>
+          {RightScreenContent("Perforce P4V Client")}
+          {RightScreenContent("GIT/SVN")}
 
           <br />
 
-          <p className="right-screen-content">
-            Visual Studios (Code/2019)
-          </p>
-          <p className="right-screen-content">
-            Laravel Framework
-          </p>
-          <p className="right-screen-content">
-            React Native
-          </p>
-          <p className="right-screen-content">
-            NPM (Windows) / Homebrew (Mac)
-          </p>
+          {RightScreenContent("Visual Studios (Code/2019)")}
+          {RightScreenContent("Laravel Framework")}
+          {RightScreenContent("React Native")}
+          {RightScreenContent("NPM (Windows) / Homebrew (Mac)")}
 
           <br />
 
-          <p className="right-screen-content">
-            C#
-          </p>
-          <p className="right-screen-content">
-            C++
-          </p>
-          <p className="right-screen-content">
-            PHP
-          </p>
-          <p className="right-screen-content">
-            JavaScript
-          </p>
-          <p className="right-screen-content">
-            Java
-          </p>
-          <p className="right-screen-content">
-            MySQL
-          </p>
+          {RightScreenContent("C#")}
+          {RightScreenContent("C++")}
+          {RightScreenContent("PHP")}
+          {RightScreenContent("JavaScript")}
+          {RightScreenContent("Java")}
+          {RightScreenContent("MySQL")}
         </Col>
       </Row>
     </>
@@ -319,22 +278,12 @@ function Hobbies() {
     <>
       <Row className="right-screen-group">
         <Col>
-          <h2 className="right-screen-header">
-            Hobbies
-          </h2>
+          {RightScreenHeader("Hobbies")}
 
-          <p className="right-screen-content">
-            Video Games
-          </p>
-          <p className="right-screen-content">
-            NERF Sports
-          </p>
-          <p className="right-screen-content">
-            Tabletop games
-          </p>
-          <p className="right-screen-content">
-            Reading about psychology
-          </p>
+          {RightScreenContent("Video Games")}
+          {RightScreenContent("NERF Sports")}
+          {RightScreenContent("Tabletop Games")}
+          {RightScreenContent("Reading about Psychology")}
         </Col>
       </Row>
     </>
@@ -346,17 +295,10 @@ function Languages() {
     <>
       <Row className="right-screen-group">
         <Col>
-          <p className="right-screen-header">
-            Languages
-          </p>
+          {RightScreenHeader("Languages")}
 
-          <p className="right-screen-content">
-            English
-          </p>
-
-          <p className="right-screen-content">
-            Malay
-          </p>
+          {RightScreenContent("English")}
+          {RightScreenContent("Malay")}
         </Col>
       </Row>
     </>
